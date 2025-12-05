93dc5fe3cbdc6ad02de26212a68509249cd9fc9d
gradform <gradform@outlook.com>
2025-12-05 08:09:11 +0300

    Revert "Feat: Implement lazy loading and preloading for pages"
    This reverts commit f6422197c58063915ec98f2f99012af5a2a4d64a.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,40 @@
-import React, { Suspense, useEffect } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import GradformAI from './components/GradformAI'; // New import
+import About from './components/About';
 import HomeAbout from './components/HomeAbout';
 import ServicesH from './components/ServicesH'; // Import ServicesH for home page
 import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
+import PlansPage from './PlansPage'; // Original ServicesPage component
 import { WorldMapDemo } from './components/WorldMapDemo';
 import InquiriesForm from './components/InquiriesForm';
 import Footer from './components/Footer';
 import Ribbons from './components/Ribbons';
 import WhatsAppButton from './components/WhatsAppButton';
+import ExplorePage from './ExplorePage'; // New import
 import ScrollToTop from './components/ScrollToTop'; // Import ScrollToTop
 
-// Lazy load page components
-const ExplorePage = React.lazy(() => import('./ExplorePage'));
-const PlansPage = React.lazy(() => import('./PlansPage'));
-const About = React.lazy(() => import('./components/About'));
-
 function App() {
-  useEffect(() => {
-    // Preload other pages when the main page loads
-    // This triggers the fetching of the component's JavaScript chunk
-    ExplorePage.preload();
-    PlansPage.preload();
-    About.preload();
-  }, []);
-
   return (
     <div className="bg-linear-to-r from-[#0000CD] via-[#0000B0] to-[#4B0082] text-white">
       <ScrollToTop /> {/* Add ScrollToTop component here */}
       <Header />
       <Ribbons />
-      <Suspense fallback={<div>Loading...</div>}> {/* Add a fallback for lazy loaded components */}
-        <Routes>
-          <Route path="/" element={
-            <main className="relative z-10">
-              <Hero />
-              <GradformAI /> {/* New component */}
-              <HomeAbout />
-              <ServicesH /> {/* MagicBento services tiles on home page */}
-              <WorldMapDemo />
-            </main>
-          } />
-          <Route path="/explore" element={<ExplorePage />} />
-          <Route path="/plans" element={<PlansPage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={
+          <main className="relative z-10">
+            <Hero />
+            <GradformAI /> {/* New component */}
+            <HomeAbout />
+            <ServicesH /> {/* MagicBento services tiles on home page */}
+            <WorldMapDemo />
+          </main>
+        } />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/plans" element={<PlansPage />} /> {/* Route to original ServicesPage */}
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
       <WhatsAppButton />
     </div>
