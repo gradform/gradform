8d0957b5a1fdae58a45b664d709895fafb28b5fe
gradform <gradform@outlook.com>
2025-12-05 07:34:39 +0300

    Fix: Remove faqs.html reference from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        faqs: resolve(__dirname, 'faqs.html'),
       },
     },
   },
