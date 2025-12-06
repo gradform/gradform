5f4e753f9580310c34a00c7be8e6fb4bb783d0c3
gradform <gradform@outlook.com>
2025-12-06 23:39:25 +0300

    Feat: Implement lazy loading for smoother navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import GradformAI from './components/GradformAI'; // New import
-import About from './components/About';
 import HomeAbout from './components/HomeAbout';
 import ServicesH from './components/ServicesH'; // Import ServicesH for home page
 import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
-import PlansPage from './PlansPage'; // Original ServicesPage component
 import { WorldMapDemo } from './components/WorldMapDemo';
 import InquiriesForm from './components/InquiriesForm';
 import Footer from './components/Footer';
 import Ribbons from './components/Ribbons';
 import WhatsAppButton from './components/WhatsAppButton';
-import ExplorePage from './ExplorePage'; // New import
 import ScrollToTop from './components/ScrollToTop'; // Import ScrollToTop
+
+// Lazy load page components
+const ExplorePage = React.lazy(() => import('./ExplorePage'));
+const PlansPage = React.lazy(() => import('./PlansPage'));
+const About = React.lazy(() => import('./components/About'));
 
 function App() {
   return (
@@ -21,20 +23,22 @@ function App() {
       <ScrollToTop /> {/* Add ScrollToTop component here */}
       <Header />
       <Ribbons />
-      <Routes>
-        <Route path="/" element={
-          <main className="relative z-10">
-            <Hero />
-            <GradformAI /> {/* New component */}
-            <HomeAbout />
-            <ServicesH /> {/* MagicBento services tiles on home page */}
-            <WorldMapDemo />
-          </main>
-        } />
-        <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/plans" element={<PlansPage />} /> {/* Route to original ServicesPage */}
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <Suspense fallback={<div>Loading page...</div>}> {/* Add a fallback for lazy loaded components */}
+        <Routes>
+          <Route path="/" element={
+            <main className="relative z-10">
+              <Hero />
+              <GradformAI /> {/* New component */}
+              <HomeAbout />
+              <ServicesH /> {/* MagicBento services tiles on home page */}
+              <WorldMapDemo />
+            </main>
+          } />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/plans" element={<PlansPage />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
       <Footer />
       <WhatsAppButton />
     </div>
